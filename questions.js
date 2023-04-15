196b4e407fc027539983dba5a6595e96214f379a
2x2Master <115193174+2x2Master@users.noreply.github.com>
2023-04-15 16:24:54 +1000

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,14 @@
+questions = [
+    {
+        "question": "Test answer.",
+        "effect": {
+            "b1": 1,
+            "b2": 1,
+            "b3": 1,
+            "b4": 1,
+            "b5": 1,
+            "b6": 1,
+            "b7": 1,
+            "b8": 1,
+        }
+    },
