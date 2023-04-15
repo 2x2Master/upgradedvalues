c3159f8191a290b96f04b9a9588e8e210ede3582
2x2Master <115193174+2x2Master@users.noreply.github.com>
2023-04-15 16:35:38 +1000

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,28 +1,28 @@
 questions = [
     {
-        "question": "Test answer 1.",
+        "question": "Test question 1",
         "effect": {
             "b1": 1,
-            "b2": 1,
+            "b2": 0,
             "b3": 1,
-            "b4": 1,
+            "b4": 0,
             "b5": 1,
-            "b6": 1,
+            "b6": 0,
             "b7": 1,
-            "b8": 1,
+            "b8": 0,
         }
     },
     {
-        "question": "Test answer 2.",
+        "question": "Test question 2",
         "effect": {
-            "b1": -1,
-            "b2": -1,
-            "b3": -1,
-            "b4": -1,
-            "b5": -1,
-            "b6": -1,
-            "b7": -1,
-            "b8": -1,
+            "b1": 0,
+            "b2": 1,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": 1,
+            "b7": 0,
+            "b8": 1,
         }
     }
 ];
