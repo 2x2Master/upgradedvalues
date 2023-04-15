7ecb6290c37d9e31ce957a89e02766690444fb3f
2x2Master <115193174+2x2Master@users.noreply.github.com>
2023-04-15 16:26:16 +1000

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        "question": "Test answer.",
+        "question": "Test answer 1.",
         "effect": {
             "b1": 1,
             "b2": 1,
@@ -12,3 +12,17 @@ questions = [
             "b8": 1,
         }
     },
+    {
+        "question": "Test answer 2.",
+        "effect": {
+            "b1": -1,
+            "b2": -1,
+            "b3": -1,
+            "b4": -1,
+            "b5": -1,
+            "b6": -1,
+            "b7": -1,
+            "b8": -1,
+        }
+    }
+];
